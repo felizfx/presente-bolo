79352958e2cd489939c2560c676465c59b3d86cd
Gabriel Silva <gabrielfelixpinho@gmail.com>
2022-10-14 18:24:05 -0300

    sala da alana

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 let valor = document.querySelector(`#resposta`);
 let botao = document.querySelector(`#enviar`);
 let conteudo = document.querySelector(`#conteudo`);
+let escondido = document.querySelector(`#escondido`);
+let controle = 0
 
 function enviarForms(){
     console.log(valor.value)
-    if(valor.value == `ovo`){
-        conteudo.innerHTML = `<h2 style='text-align: center; margin-top: 10px; color: green;'>Procura atras da geladeria ;)</h2>`;
+    valor.value
+    if(valor.value == `max`){
+        conteudo.innerHTML = `<h2 style='text-align: center; margin-top: 10px; color: green;'>Parabens Max ;)</h2>`;
     } else {
-        conteudo.innerHTML = `<h2 style='text-align: center; margin-top: 10px; color: red;'>ERRADO</h2>`;
+        conteudo.innerHTML = `<h2 style='text-align: center; margin-top: 10px; color: red;'>MACACO</h2>`;
     }
+    controle++
+    if(controle == 5){
+        escondido.style.left = '-5px'
+        escondido.style.transition = '2s'
+    }   
 }
 
